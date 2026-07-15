5427780ee4f81f6b374b204d79ffa8ef6b2f08cc
b0tguy <117023176+b0tguy@users.noreply.github.com>
2026-07-15 06:54:44 +0200

    Update CONFIG to use dynamic backend host

--- src/static/scripts/config.js
+++ src/static/scripts/config.js
@@ -1,6 +1,8 @@
+const BACKEND_HOST = import.meta.env.VITE_BACKEND_HOST;
+
 export const CONFIG = {
-  bUrl: '/seal/',
-  ws: '/wisp/',
+  bUrl: BACKEND_HOST ? `https://${BACKEND_HOST}/seal/` : '/seal/',
+  ws: BACKEND_HOST ? `wss://${BACKEND_HOST}/wisp/` : '/wisp/',
   unsupported: ['spotify.com'],
   filter: ['neal.fun'],
 };
